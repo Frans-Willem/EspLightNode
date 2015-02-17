ba90bf1456e66e8d362b13680eaa51b0e32d9d99
Scott Franzyshen <sfranzyshen@gmail.com>
2015-02-17 07:46:06 -0800

    Update tpm2net.c: Added support for "multiple block" frames
    The tpm2net protocol supports splitting the data frame over several packets. This is done to avoid network packet fragmentation when the (ie. 512 rgb leds) frame size approches the network mtu. This patch supports tpm2net "multiple block" frames.

--- user/input_protocols/tpm2net.c
+++ user/input_protocols/tpm2net.c
@@ -12,24 +12,33 @@
 
 #include "../output_protocols/ws2801.h"
 
+uint16_t framebuffer_len = 0;
+unsigned char framebuffer[1536]; //max 512 rgb pixels
+
 static void ICACHE_FLASH_ATTR tpm2net_recv(void *arg, char *pusrdata, unsigned short length) {
-	unsigned char *data =(unsigned char *)pusrdata;
-	if (data && length>=6 && data[0]==0x9C) {
-		uint8_t blocktype = data[1];
-		uint16_t framelength = ((uint16_t)data[2] << 8) | (uint16_t)data[3];
-		uint8_t packagenum = data[4];
-		uint8_t numpackages = data[5];
-		if (length >= framelength + 7 && data[6+framelength]==0x36) {
-			unsigned char *frame = &data[6];
-			if (blocktype == 0xDA) {
-				ws2801_strip(frame,framelength);
-			}
-		} else {
-			//Invalid length or invalid end byte
-		}
-	} else {
-		//Not a valid header
-	}
+    unsigned char *data =(unsigned char *)pusrdata; //pointer to espconn's returned data
+    if (data && length >= 6 && data[0]==0x9C) { // header identifier (packet start)
+        uint8_t blocktype = data[1]; // block type
+        uint16_t framelength = ((uint16_t)data[2] << 8) | (uint16_t)data[3]; // frame length
+        uint8_t packagenum = data[4]; // packet number 0-255 (0x00 = no split)
+        uint8_t numpackages = data[5]; // total packets 1-255
+        if (blocktype == 0xDA) { // data command ...
+            if (length >= framelength + 7 && data[6+framelength]==0x36) { // header end (packet stop)
+                if (packagenum == 0x01 && numpackages == 0x01) { // no frame split found
+                    unsigned char *frame = &data[6]; // pointer 'frame' to espconn's data (start of data)
+                    ws2801_strip(frame, framelength); // send data to strip
+                } else { //frame split is found
+                    os_memcpy (&framebuffer[framebuffer_len], &data[6], framelength);
+                    framebuffer_len += framelength;
+                    if (packagenum == numpackages) { // all packets found 
+                        unsigned char *frame = &framebuffer[0]; // pointer 'frame' framebuffer
+                        ws2801_strip(frame, framebuffer_len); // send data to strip
+                        framebuffer_len = 0;
+                    }
+                }
+            }
+        }
+    }
 }
 
 void tpm2net_init() {
