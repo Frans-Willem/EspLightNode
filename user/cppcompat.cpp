4500bd9850124a0ff3366b60d9a55a5ae89e587a
Frans-Willem Hardijzer <fw@hardijzer.nl>
2015-09-13 20:33:28 +0200

    Added stubs to cppcompat to catch some unknown linked in symbols when using std::string

--- user/cppcompat.cpp
+++ user/cppcompat.cpp
@@ -16,3 +16,15 @@ void operator delete(void *ptr) {
 extern "C" void __cxa_pure_virtual() {
 	while (1);
 }
+
+// We should actually find out why this is linked in, but for now, stubs
+extern "C" void _sbrk_r() { while(1); }
+extern "C" void _write_r() { while(1); }
+extern "C" void _read_r() { while(1); }
+extern "C" void _fstat_r() { while(1); }
+extern "C" void _exit() { while(1); }
+extern "C" void _kill_r() { while(1); }
+extern "C" void _open_r() { while(1); }
+extern "C" void _close_r() { while(1); }
+extern "C" void _getpid_r() { while(1); }
+extern "C" void _lseek_r() { while(1); }
