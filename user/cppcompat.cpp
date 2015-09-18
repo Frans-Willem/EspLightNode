f4aa74ea726ffcd05ea41103bb00a932a6d6a40d
Frans-Willem Hardijzer <fw@hardijzer.nl>
2015-09-18 22:11:48 +0200

    Some more stubs

--- user/cppcompat.cpp
+++ user/cppcompat.cpp
@@ -17,19 +17,55 @@ extern "C" void __cxa_pure_virtual() {
 	while (1);
 }
 
-// We should actually find out why this is linked in, but for now, stubs
-/*
-extern "C" void _sbrk_r() { while(1); }
+// These make no sense at all on ESP8266, but just in case something links to it here are stubs
 extern "C" void _write_r() { while(1); }
 extern "C" void _read_r() { while(1); }
 extern "C" void _fstat_r() { while(1); }
-extern "C" void _exit() { while(1); }
-extern "C" void _kill_r() { while(1); }
 extern "C" void _open_r() { while(1); }
 extern "C" void _close_r() { while(1); }
-extern "C" void _getpid_r() { while(1); }
 extern "C" void _lseek_r() { while(1); }
-*/
+
+// Stub
 extern "C" void abort() {
 	while (1);
 }
+
+// All different versions of malloc, free, realloc, etc
+extern "C" void* _malloc_r(struct _reent *ignored __attribute__((unused)),
+		size_t size) {
+	return os_zalloc(size);
+}
+
+extern "C" void _free_r(struct _reent *ignore __attribute__((unused)), void *ptr) {
+	return os_free(ptr);
+}
+
+extern "C" void* _calloc_r(struct _reent *ignored __attribute((unused)), size_t x, size_t y) {
+	return os_zalloc(x*y);
+}
+
+extern "C" void* _realloc_r(struct _rrent *ignore __attribute__((unused)), void *ptr, size_t size) {
+	os_free(ptr);
+	return os_zalloc(size);
+}
+
+extern "C" void* malloc(size_t size) {
+	return os_zalloc(size);
+}
+
+extern "C" void free(void *ptr) {
+	return os_free(ptr);
+}
+
+extern "C" void* realloc(void *ptr, size_t size) {
+	os_free(ptr);
+	return os_zalloc(size);
+}
+
+// Relatively 'big' functions that are implemented in ROM,
+// We save space by redirecting to the ROM versions.
+extern "C" void *memcpy(void *dest, const void *src, size_t n) {
+	return ets_memcpy(dest, src, n);
+}
+
+// strcpy, strncpy, strlen, memset are already provided by ESP8266 linker script
